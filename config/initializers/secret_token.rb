6129d8dc11b3e79e7bc96f2b34905df88670bce9
Yihuan Zhou <yihuan.zhou@gmail.com>
2012-10-13 14:54:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMUEnactus::Application.config.secret_token = '741c1988840f7ac22df17fd17dcc587063018f4c075adbeb8c5947998cec8869831cf0a24b7ab46fb87e9c609a1c73753cbf77d47c6926159170248c9f5145f7'
